05550b0a5186617852d9702816db8af98adeb168
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-13 11:04:09 -0800

    Brew formula update for ghost version v0.6.0

--- ghost.rb
+++ ghost.rb
@@ -2,16 +2,16 @@
 class Ghost < Formula
   desc "https://github.com/ghost-language/ghost."
   homepage ""
-  version "0.5.0"
+  version "0.6.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ghost-language/ghost/releases/download/v0.5.0/ghost_0.5.0_Darwin_x86_64.tar.gz"
-    sha256 "2d8680a77a1f5d30c5a533920dffa2eb1767d2ca2ace51fb602d11f2e1d5242b"
+    url "https://github.com/ghost-language/ghost/releases/download/v0.6.0/ghost_0.6.0_Darwin_x86_64.tar.gz"
+    sha256 "c02a24ddf4947a9e556ca5bb363af0d22877cabc8395e20c01efed7cc7cef9fe"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/ghost-language/ghost/releases/download/v0.5.0/ghost_0.5.0_Linux_x86_64.tar.gz"
-      sha256 "d28696e2d10a9f3644ccdc44751dd174ce07b8c174c428c1e0b15a0918048676"
+      url "https://github.com/ghost-language/ghost/releases/download/v0.6.0/ghost_0.6.0_Linux_x86_64.tar.gz"
+      sha256 "ff99c35c9eef87c01e2880234cd6236cb6dbbe6d41f040e65c438cb198846672"
     end
   end
   
